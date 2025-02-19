afc7e1f3b82b681e15690f7edee679c1d525348d
Amadrid <antonioandres.madridbroce@student.griffith.ie>
2025-02-19 13:10:54 +0000

    Added files

---
+++ src/griffith/Grades.java
@@ -0,0 +1,30 @@
+package griffith;
+
+public class Grades {
+
+	
+		
+		int gradesMax(int[] grades) {
+			
+			return 0;
+		}
+		
+		int gradesTotal (int[] grades){
+			
+			return 0;
+			
+		}// get sum of array
+		
+		double gradesAverage(int[] grades) {
+			return 0;
+		} // get average of array
+		
+		
+		int countFails(int[] grades, int minGrade) {
+			return 0;
+			
+		} // count how many grades < minGrade
+		
+	}
+
+
